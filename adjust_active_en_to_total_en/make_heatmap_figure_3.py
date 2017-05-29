f0f1a76a6d35da7e4539dfa747960a25e5bc36be
annashcherbina <annashch@stanford.edu>
2017-05-29 06:49:07 -0700

    paper version of energy error calculation script

--- adjust_active_en_to_total_en/make_heatmap_figure_3.py
+++ adjust_active_en_to_total_en/make_heatmap_figure_3.py
@@ -1,5 +1,50 @@
 import pdb
 
+def make_metadata_dict(data):
+    header=data[0].split('\t')
+    metadata_dict=dict()
+    for line in data[1::]:
+        tokens=line.split('\t')
+        subject=tokens[0]
+        metadata_dict[subject]=dict()
+        for i in range(1,len(tokens)):
+            field=header[i]
+            value=tokens[i]
+            metadata_dict[subject][field]=value
+    return metadata_dict 
+
+def roza_shizgal(active_energy,subject_metadata):
+    age=float(subject_metadata['Age'])
+    height=float(subject_metadata['Height'])
+    weight=float(subject_metadata['Weight'])
+    sex=subject_metadata['Sex']
+    
+    if sex=="Male":
+        bmr=88.362+(13.397*weight)+(4.799*height)-(5.677*age)
+    elif sex=="Female":
+        bmr=447.593+(9.247*weight)+(3.098*height)-(4.330*age)
+    else:
+        raise Exception("Invalid entry!:"+str(subject_metadata))
+    return  (bmr/1440.0)+ active_energy
+
+
+
+#Harris Benedict equation revised by Mifflin and St. Jeor in 1990
+def harris_benedict(active_energy,subject_metadata):
+    age=float(subject_metadata['Age'])
+    height=float(subject_metadata['Height'])
+    weight=float(subject_metadata['Weight'])
+    sex=subject_metadata['Sex']    
+    if sex=="Male":
+        bmr=(10*weight)+(6.25*height)-(5*age)+5
+    elif sex=="Female":
+        bmr=(10*weight)+(6.25*height)-(5*age)-161
+    else:
+        raise Exception("Invalid entry!:"+str(subject_metadata))
+    return (bmr/1440.0)+active_energy 
+
+
+
 def convert_state(state):
     if state=="walk1":
         return "walk1"
@@ -20,8 +65,15 @@ def convert_state(state):
     else:
         return None 
 
-batch1=open('Batch_1_full_dataset.tsv','r').read().strip().split('\n')
-batch2=open('Batch_2_full_dataset.tsv.WITHENERGY','r').read().strip().split('\n')
+batch1=open('../raw_data/Batch_1_full_dataset.tsv','r').read().strip().split('\n')
+batch2=open('../raw_data/Batch_2_full_dataset.tsv.WITHENERGY','r').read().strip().split('\n')
+metadata1=open("../raw_data/Batch1_metadata.csv",'r').read().strip().split('\n')
+metadata2=open("../raw_data/Batch2_metadata.csv",'r').read().strip().split('\n')
+metadata=metadata1+metadata2[1::]
+metadata_dict=make_metadata_dict(metadata)
+
+#build metadata dictionary
+
 placeholder="NA" # value to use instead of NA so that clustering in R and heatmap.2 is possible! 
 #get the delta from gold standard for each activity
 
@@ -73,9 +125,19 @@ for line in batch1[1::]:
                 continue
             device_value=float(device_value)
             if gold_standard_value==0:
-                continue 
+                continue
+            #get the corrected device value!!!
+            roza_shizgal_corrected=roza_shizgal(device_value,metadata_dict[subject])
+            harris_benedict_corrected=harris_benedict(device_value,metadata_dict[subject])
             device_error=(device_value-gold_standard_value)/gold_standard_value
+            rs_device_error=(roza_shizgal_corrected-gold_standard_value)/gold_standard_value 
+            hb_device_error=(harris_benedict_corrected-gold_standard_value)/gold_standard_value
             device_error=round(device_error,3)
+            rs_device_error=round(rs_device_error,3)
+            hb_device_error=round(hb_device_error,3)
+            kcal_error=device_value-gold_standard_value
+            rs_kcal_error=roza_shizgal_corrected-gold_standard_value
+            hb_kcal_error=harris_benedict_corrected-gold_standard_value
             #device_error=abs(device_error)
             if subject not in device_activity_mean:
                 device_activity_mean[subject]=dict() 
@@ -84,7 +146,8 @@ for line in batch1[1::]:
             if metric not in device_activity_mean[subject][device]: 
                 device_activity_mean[subject][device][metric]=dict()
             if curstate not in device_activity_mean[subject][device][metric]:
-                device_activity_mean[subject][device][metric][curstate]=device_error
+                device_activity_mean[subject][device][metric][curstate]=[device_error,rs_device_error,hb_device_error,kcal_error,rs_kcal_error,hb_kcal_error]
+                
 #repeat this same analysis for batch 2
 #print str(device_activity_mean)
 print "BATCH 2"
@@ -138,9 +201,20 @@ for line in batch2[1::]:
                 continue
             device_value=float(device_value)
             if gold_standard_value==0:
-                continue 
+                continue
+            roza_shizgal_corrected=roza_shizgal(device_value,metadata_dict[subject])
+            harris_benedict_corrected=harris_benedict(device_value,metadata_dict[subject])
             device_error=(device_value-gold_standard_value)/gold_standard_value
-            device_error=round(device_error,3) 
+            device_error=round(device_error,3)
+            rs_device_error=(roza_shizgal_corrected-gold_standard_value)/gold_standard_value 
+            hb_device_error=(harris_benedict_corrected-gold_standard_value)/gold_standard_value
+            device_error=round(device_error,3)
+            rs_device_error=round(rs_device_error,3)
+            hb_device_error=round(hb_device_error,3)
+            kcal_error=device_value-gold_standard_value
+            rs_kcal_error=roza_shizgal_corrected-gold_standard_value
+            hb_kcal_error=harris_benedict_corrected-gold_standard_value
+            
             #device_error=abs(device_error) 
             if subject not in device_activity_mean:
                 device_activity_mean[subject]=dict() 
@@ -149,7 +223,7 @@ for line in batch2[1::]:
             if metric not in device_activity_mean[subject][device]: 
                 device_activity_mean[subject][device][metric]=dict()
             if curstate not in device_activity_mean[subject][device][metric]:
-                device_activity_mean[subject][device][metric][curstate]=device_error
+                device_activity_mean[subject][device][metric][curstate]=[device_error,rs_device_error,hb_device_error,kcal_error,rs_kcal_error,hb_kcal_error]
 
 
 print("FORMATTING DATA FOR HEATMAP OUTPUT!!")
@@ -194,7 +268,9 @@ outf_en=open('en.heatmap.bySubject','w')
 outf_en.write('Subject')
 for state in states:
     for device in devices:
-        outf_en.write('\t'+state+"_"+device)
+        for metric in ['percent','kcal']: 
+            for correction in ['uncorrected','RozaShizgal','HarrisBenedict']:
+                outf_en.write('\t'+state+"_"+device+'_'+metric+'_'+correction)
 outf_en.write('\n')
 
 for subject in subjects:
@@ -204,13 +280,13 @@ for subject in subjects:
             if device in device_activity_mean[subject]:
                 try:
                     if state in device_activity_mean[subject][device]['Energy']:
-                        outf_en.write('\t'+str(device_activity_mean[subject][device]['Energy'][state]))
+                        outf_en.write('\t'+'\t'.join([str(i) for i in device_activity_mean[subject][device]['Energy'][state]]))
                     else:
-                        outf_en.write('\t'+placeholder)
+                        outf_en.write('\t'+'\t'.join([placeholder]*3))
                 except:
-                    outf_en.write('\t'+placeholder) 
+                    outf_en.write('\t'+'\t'.join([placeholder]*3)) 
             else:
-                outf_en.write('\t'+placeholder) 
+                outf_en.write('\t'+'\t'.join([placeholder]*3)) 
     outf_en.write('\n')
     
 
